2648e970b3ae83a0fcad8c09c9abe9d62223e4be
João Pedro <55984774+joaopver10@users.noreply.github.com>
2020-05-26 22:02:26 -0300

    update formulário

--- Portal Projeto/js/cadastroscript.js
+++ Portal Projeto/js/cadastroscript.js
@@ -9,9 +9,7 @@ if (form.addEventListener) {
 function validaCadastro(evt){
 	var nome = document.getElementById('nome');
 	var email = document.getElementById('email');
-	var sexo = document.getElementById('sexo');
-	var senha = document.getElementById('senha');
-	var senha2 = document.getElementById('senha2');
+	var texto = document.getElementById('comment');
 	var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 	var contErro = 0;
  
@@ -40,49 +38,15 @@ function validaCadastro(evt){
 		contErro += 1;
 	}	
  
-	/* Validação do campo sexo */
-	caixa_sexo = document.querySelector('.msg-sexo');
-	if(sexo.value == ""){
-		caixa_sexo.innerHTML = "Favor preencher o Sexo";
-		caixa_sexo.style.display = 'block';
-		contErro += 1;
-	}else{
-		caixa_sexo.style.display = 'none';
-	}
- 
-	/* Validação do campo senha */
-	caixa_senha = document.querySelector('.msg-senha');
-	if(senha.value == ""){
-		caixa_senha.innerHTML = "Favor preencher a Senha";
-		caixa_senha.style.display = 'block';
-		contErro += 1;
-	}else if(senha.value.length < 6){
-		caixa_senha.innerHTML = "Favor preencher a Senha com o mínimo de 6 caracteres";
-		caixa_senha.style.display = 'block';
+
+	/* Validação do campo texto */
+	caixa_texto = document.querySelector('.msg-texto');
+	if(texto.value.length == ""){
+		caixa_texto.innerHTML = "Favor preencher o texto antes de enviar";
+		caixa_texto.style.display = 'block';
 		contErro += 1;
 	}else{
 		caixa_senha.style.display = 'none';
-	}
- 
-	/* Validação do campo repita a senha */
-	caixa_senha2 = document.querySelector('.msg-senha2');
-	if(senha2.value == ""){
-		caixa_senha2.innerHTML = "Favor preencher o campo Repita a Senha";
-		caixa_senha2.style.display = 'block';
-		contErro += 1;
-	}else if(senha2.value.length < 6){
-		caixa_senha2.innerHTML = "Favor preencher o campo Repita a Senha com o mínimo de 6 caracteres";
-		caixa_senha2.style.display = 'block';
-		contErro += 1;
-	}else{
-		caixa_senha2.style.display = 'none';
-	}
- 
-	/* Valida se a senha é igual ao campo repita a senha */
-	if(senha.value != "" && senha2.value != "" && senha.value != senha2.value){
-		caixa_senha2.innerHTML = "O campo Repita a Senha é diferente do campo Senha";
-		caixa_senha2.style.display = 'block';
-		contErro += 1;
 	}
  
 	if(contErro > 0){
